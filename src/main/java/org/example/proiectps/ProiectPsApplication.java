89f7c0f721ca7a731abfea4de606f8a94c59dc56
alexasv14 <svegleralexa@gmail.com>
2026-03-18 18:10:59 +0200

    Creare proiect

---
+++ src/main/java/org/example/proiectps/ProiectPsApplication.java
@@ -0,0 +1,13 @@
+package org.example.proiectps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectPsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectPsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/proiectps/ProiectPsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.proiectps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectPsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
