bef684c83853c2fd7914aa29bbafddc1aece6011
toadersidonia <toadersidonia22@gmail.com>
2026-04-03 01:02:09 +0300

    Codul pentru comentarii

--- src/main/java/org/example/proiectps/repository/CommentRepository.java
+++ src/main/java/org/example/proiectps/repository/CommentRepository.java
@@ -4,7 +4,11 @@ import org.example.proiectps.entity.Comment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CommentRepository extends CrudRepository<Comment, Long> { }
+public interface CommentRepository extends CrudRepository<Comment, Long> {
+    List<Comment> findByPostPostId(Long postId);
+    //metodele default in crud repo sunt save(), findById(), findAll(), deleteById()
+}
