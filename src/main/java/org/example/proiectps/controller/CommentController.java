97f97297092dff70e0dd3bf27e79b13fbdffeb78
toadersidonia <toadersidonia22@gmail.com>
2026-04-03 02:33:01 +0300

    modif comment

--- src/main/java/org/example/proiectps/controller/CommentController.java
+++ src/main/java/org/example/proiectps/controller/CommentController.java
@@ -15,7 +15,7 @@ public class CommentController {
     private CommentService commentService;
 
     @PostMapping
-    public Comment createComment(@RequestBody Long postId, @RequestParam Long userId, @RequestParam Comment comment) {
+    public Comment createComment(@RequestParam Long postId, @RequestParam Long userId, @RequestBody Comment comment) {
         return commentService.createComment(comment, postId, userId);
     }
 
